98b94808fcc0ab4d4f259584033944960cb5693a
cameliapatilea <cami.patilea@gmail.com>
2020-05-23 20:51:13 +0300

    Finished upload

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,10 @@
 import React from "react";
 
 // reactstrap components
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 // core components
+import ImageUpload from "components/ImageUpload.js";
 
 function IndexHeader() {
   return (
@@ -13,11 +14,11 @@ function IndexHeader() {
         className="page-header section-dark"
         style={{
           backgroundImage:
-            "url(" + require("assets/img/antoine-barres.jpg") + ")"
+            "url(" + require("assets/img/antoine-barres.jpg") + ")",
         }}
       >
         <div className="filter" />
-        <div className="content-center" style={{marginTop:"-30%"}}>
+        <div className="content-center" >
           <Container>
             <div className="title-brand">
               {/* <h1 className="presentation-title">Paper Kit React</h1> */}
@@ -25,12 +26,21 @@ function IndexHeader() {
             <h2 className="presentation-subtitle text-center">
               Upload one image and let us guess your emotion!
             </h2>
+            <br />
+            <br />
+            <Row>
+              <Col md="3" sm="2"></Col>
+              <Col md="6" sm="8">
+                <ImageUpload />
+              </Col>
+              <Col md="3" sm="2"></Col>
+            </Row>
           </Container>
         </div>
         <div
           className="moving-clouds"
           style={{
-            backgroundImage: "url(" + require("assets/img/clouds.png") + ")"
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
           }}
         />
       </div>
